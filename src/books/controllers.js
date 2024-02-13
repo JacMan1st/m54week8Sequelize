cdfd5f1eef2bea20f6c4b90d897944287c4a85d6
Jack Mears <jack.mears@ymail.com>
2024-02-13 16:29:25 +0000

    enhancement: add get all books

---
+++ src/books/controllers.js
@@ -0,0 +1,29 @@
+const Book = require("./model");
+
+const addBook = async (req, res) => {
+  try {
+    const book = await Book.create({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+    });
+
+    res.status(201).json({ message: `${book.title} was added.`, book: book });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.findAll({});
+    res.send({ message: "got all books successfully", books: books });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+module.exports = {
+  addBook: addBook,
+  getAllBooks: getAllBooks,
+};
