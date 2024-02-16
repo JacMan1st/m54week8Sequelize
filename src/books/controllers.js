1737e5077c47d0970101492819271c3772517820
Jack Mears <jack.mears@ymail.com>
2024-02-16 13:28:26 +0000

    enhancement: add delete single book by title and delete all books

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -42,8 +42,55 @@ const dynamicUpdate = async (req, res) => {
   }
 };
 
+const deleteBookByTitle = async (req, res) => {
+  try {
+    const { title } = req.body;
+
+    const book = await Book.findOne({ where: { title: title } });
+
+    if (!book) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+    await book.destroy();
+    res.status(200).json({ message: "Book deleted", deletedBook: book });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+const deleteAllBooks = async (req, res) => {
+  try {
+    const deletedBooks = await Book.destroy({ where: {} });
+
+    if (deletedBooks === 0) {
+      return res.status(404).json({ message: "No books found to delete" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "All books deleted", deletedBooks: deletedBooks });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+// const deleteAllBooks2 = async (req, res) => {
+//   try {
+//     await Book.destroy({
+//       where: {},
+//       truncate: true,
+//     });
+
+//     res.status(200).json({ message: "All books deleted successfully" });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message, error: error });
+//   }
+// };
+
 module.exports = {
   addBook: addBook,
   getAllBooks: getAllBooks,
   dynamicUpdate: dynamicUpdate,
+  deleteBookByTitle: deleteBookByTitle,
+  deleteAllBooks: deleteAllBooks,
 };
--- src/books/routes.js
+++ src/books/routes.js
@@ -1,10 +1,18 @@
 const { Router } = require("express");
 const bookRouter = Router();
 
-const { addBook, getAllBooks, dynamicUpdate } = require("./controllers");
+const {
+  addBook,
+  getAllBooks,
+  dynamicUpdate,
+  deleteBookByTitle,
+  deleteAllBooks,
+} = require("./controllers");
 
 bookRouter.post("/books/addBook", addBook);
 bookRouter.get("/books/getAllBooks", getAllBooks);
 bookRouter.put("/books/dynamicUpdate", dynamicUpdate);
+bookRouter.delete("/books/deleteBookByTitle", deleteBookByTitle);
+bookRouter.delete("/books/deleteAllBooks", deleteAllBooks);
 
 module.exports = bookRouter;
